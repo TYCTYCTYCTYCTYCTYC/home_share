7173fd96e887cb016e54ccbdd946d5d877d8cd9a
iamdevastated <tanyixuan@1utar.my>
2023-04-14 14:30:01 +0800

    fix bug to handle registered but hvt join home

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'package:home_share/pages/register_page.dart';
 import 'package:home_share/utils/constants.dart';
 import 'package:home_share/home.dart';
+import 'package:home_share/main.dart';
+import 'package:home_share/pages/create_or_join.dart';
 import 'package:home_share/pages/login_page.dart';
-
 
 /// Page to redirect users to the appropriate page depending on the initial auth state
 class SplashPage extends StatefulWidget {
@@ -25,13 +27,24 @@ class SplashPageState extends State<SplashPage> {
     // await for for the widget to mount
     await Future.delayed(Duration.zero);
 
-    final session = supabase.auth.currentSession;
+    final session = Supabase.instance.client.auth.currentSession;
     if (session == null) {
       Navigator.of(context)
           .pushAndRemoveUntil(LoginPage.route(), (route) => false);
     } else {
-      Navigator.of(context)
-          .pushAndRemoveUntil(Home.route(), (route) => false);
+      final homeQuery = await Supabase.instance.client
+          .from('user_home')
+          .select()
+          .eq('user_id', Supabase.instance.client.auth.currentUser!.id)
+          .execute();
+
+      if (homeQuery.data.length == 0) {
+        Navigator.of(context)
+            .pushAndRemoveUntil(CreateOrJoin.route(), (route) => false);
+      } else {
+        Navigator.of(context)
+            .pushAndRemoveUntil(Home.route(), (route) => false);
+      }
     }
   }
 
